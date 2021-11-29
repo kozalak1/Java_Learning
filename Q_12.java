d70202afce0af8c8d8c3a734376fc7a418250567
mehmet surucu <mehmetsmarket@gmail.com>
2021-11-30 00:15:24 +0300

    30 Nov

---
+++ Q_12.java
@@ -0,0 +1,30 @@
+package Java_Learning;
+
+public class Q_12 {
+
+	public static void main(String[] args) {
+	/*	Verilen iki sayının (sıfırdan büyük veya sıfıra eşit) toplamını hesaplayan ve yazdıran bir Java programı yazın.
+	 *  Verilen tamsayılar veya toplam 10'dan fazla basamakli olursa, "OverFlow" yazdırın.
+
+		Ornek:
+
+		INPUT :
+
+		25
+
+		46
+
+		OUTPUT :
+
+		Numaralarin Toplami:
+
+		71
+			*/
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ Q_14.java
@@ -0,0 +1,24 @@
+package Java_Learning;
+
+public class Q_14 {
+
+	public static void main(String[] args) {
+	/*	Bir aydaki gün sayısını hesaplayan bir Java programı yazınız.
+
+		INPUT:
+
+		Ay Numarasi:
+
+		2
+
+		Yil :
+
+		2016
+
+		OUTPUT :
+
+		Subat 2016 29 Gundur. */
+
+	}
+
+}
