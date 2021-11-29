bebb45377252c2d05a1e29e7786c8f51c0abe253
mehmet surucu <mehmetsmarket@gmail.com>
2021-11-29 13:49:04 +0300

    28 Nov

---
+++ Q_10.java
@@ -0,0 +1,21 @@
+package Java_Learning;
+
+public class Q_10 {
+
+	public static void main(String[] args) {
+		/* Girilen zamanı saniyeye çeviren bir program yazınız.
+
+		Örnek Çıktı:
+
+		1 saat 10 dakika 50 saniye ==>
+
+		4250 saniye
+*/
+		
+		
+		
+		
+		
+	}
+
+}
